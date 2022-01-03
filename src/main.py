25aaa6b5233b8ea322f5e8c163a666b6e6a0e46e
fairwell-coding <coding@temmel.me>
2022-01-03 14:38:03 +0100

    refactoring & network ready including forward

--- src/main.py
+++ src/main.py
@@ -6,9 +6,8 @@ from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import json
 
-from scipy.special import softmax
 
-
+# Train & test data sets
 x_train_g = np.empty((280, 4))
 y_train_g = np.empty((280, ))
 x_test_g = np.empty((40, 4))
@@ -26,53 +25,82 @@ class NN(object):
         self.init_params()
 
     def init_params(self):
-        self.theta = {'W0': normal(0, 0.05, (self.num_hidden, self.num_input)),
-                      'W1': normal(0, 0.05, (self.num_output, self.num_hidden)),
-                      'b0': normal(0, 0.05, self.num_hidden),
-                      'b1': normal(0, 0.05, self.num_output)}
+        self.theta = []
+        self.theta.append({'W0': normal(0, 0.05, (self.num_hidden, self.num_input)),
+                           'b0': normal(0, 0.05, self.num_hidden)})  # hidden layer
+        self.theta.append({'W1': normal(0, 0.05, (self.num_output, self.num_hidden)),
+                           'b1': normal(0, 0.05, self.num_output)})  # output layer
 
     @staticmethod
-    def softmax(self, z2: np.ndarray):
-        ''' Non-linear output activation function, i.e. softmax for multi-class classification, which outputs the probabilities for each class w.r.t. the sample.
+    def softmax(x: np.ndarray):
+        """ Non-linear output activation function, i.e. softmax for multi-class classification, which outputs the probabilities for each class w.r.t. the sample.
 
-        :param z2: pre-activation input for output-layer
-        :return: softmax output (probabilities of all classes which sum to 1)
-        '''
+        param x: pre-activation input for output-layer
+        return: softmax output (probabilities of all classes which sum to 1)
+        """
 
-        return np.exp(z2) / np.sum(np.exp(z2))
+        return np.exp(x) / np.sum(np.exp(x))
 
     @staticmethod
-    def activation_h(z: np.ndarray):
-        return np.log(1 + exp(z))
+    def softplus(x: np.ndarray):
+        """ Softplus activation function used for hidden layer.
 
-    def forward(self, x: np.ndarray):
-        self.x = x
-        self.z1 = self.theta['W0'] @ x + self.theta['b0']
-        self.a1 = self.activation_h(self.z1)
-        self.z2 = self.theta['W1'] @ self.a1 + self.theta['b1']
-        self.a2 = softmax(self.z2)
+        param x: preactivation value of neurons in hidden layer
+        return: calculated softplus value (activated value of hidden layer)
+        """
+        return np.log(1 + exp(x))
 
-        return self.a2
+    def train(self, lr, epochs):
+        for epoch in range(epochs):
+            for train_sample in x_train_g:
+                y_hat = self.forward(train_sample)
+                self.backward()
 
-    def backward(self, y, y_hat):
+    def predict(self, x: np.ndarray):
+        return self.forward(x, store_predictions=False)
+
+    def forward(self, x: np.ndarray, store_predictions=True):
+        self.theta[0]['z1'] = self.theta[0]['W0'] @ x + self.theta[0]['b0']
+        self.theta[0]['a1'] = self.softplus(self.theta[0]['z1'])
+        self.theta[1]['z2'] = self.theta[1]['W1'] @ self.theta[0]['a1'] + self.theta[1]['b1']
+        self.theta[1]['a2'] = self.softmax(self.theta[1]['z2'])
+
+        return self.theta[1]['a2']
+
+    def backward(self):
+        """ Perform backwards pass (backpropagate errors using partial derivatives via chain-rule)
+        """
+
+
+
+        print('x')
+
+    def backward_old(self, y, y_hat):
+        """
+
+        :param y: target label for supervised learning
+        :param y_hat: predicted value
+        :return:
+        """
+
         del_L_a2 = - np.divide(y, y_hat)  # derivative of loss w.r.t. output activation function
-        del_a2_z2 = softmax(self.z2) - softmax(self.z2)**2
+        del_a2_z2 = self.softmax(self.z2) - self.softmax(self.z2)**2
         del_z2_W1 = self.a1
         del_z2_a1 = self.theta['W1']
         del_a1_z1 = np.exp(self.z1) / (1 + np.exp(self.z1))
         del_z1_W0 = self.x
 
-        del_W1 = np.matmul((del_L_a2 * del_a2_z2).reshape(self.num_output, 1), del_z2_W1.T.reshape(1, self.num_hidden))
-        del_b1 = del_L_a2 * del_a2_z2
-        del_W0 = del_L_a2 * del_a2_z2 * del_z2_a1 * del_a1_z1 * del_z1_W0
-        del_b0 = del_L_a2 * del_a2_z2 * del_z2_a1 * del_a1_z1
+        # del_W1 = np.matmul((del_L_a2 * del_a2_z2).reshape(self.num_output, 1), del_z2_W1.T.reshape(1, self.num_hidden))
+        # del_b1 = del_L_a2 * del_a2_z2
+        # del_W0 = del_L_a2 * del_a2_z2 * del_z2_a1 * del_a1_z1 * del_z1_W0
+        # del_b0 = del_L_a2 * del_a2_z2 * del_z2_a1 * del_a1_z1
 
         print('x')
 
-
     def export_model(self):
         with open(f'model_{self.gradient_method}.json', 'w') as fp:
-            json.dump({key: value.tolist() for key, value in self.theta.items()}, fp)
+            for layer in self.theta:
+                json.dump({key: value.tolist() for key, value in layer.items()}, fp)
 
 
 def task1():
@@ -83,52 +111,58 @@ def task1():
             - ax[1] Plot showing the training and test accuracy for both variants
     """
 
+    # Define network parameters
     num_input = 4
     num_hidden = 16
     num_output = 3
 
-    # Create the models
-    # Model using steepest descent
-    net_GD = NN(num_input, num_hidden, num_output, gradient_method='GD')
-    y_hat = net_GD.forward(x_test_g[0, :])
-    net_GD.backward(y_train_g[0], y_hat)
+    net_GD = NN(num_input, num_hidden, num_output, gradient_method='GD')  # create NN with the steepest gradient descent
+    # y_hat = net_GD.forward(x_test_g[0, :])
+    # net_GD.backward(y_train_g[0], y_hat)
+    net_GD.train(lr=0.01, epochs=350)  # lr = {0.1, 0.001}
 
     # Model using Nesterovs method
-    net_NAG = NN(num_input, num_hidden, num_output, gradient_method='NAG')
+    # net_NAG = NN(num_input, num_hidden, num_output, gradient_method='NAG')
 
+    # Export models
     net_GD.export_model()
-    net_NAG.export_model()
+    # net_NAG.export_model()
 
+    return __create_plots()
+
+
+def __create_plots():
     # Configure plot
-    fig = plt.figure(figsize=[12,6])
+    fig = plt.figure(figsize=[12, 6])
     axs = []
     axs.append(fig.add_subplot(121))
     axs.append(fig.add_subplot(122))
-
     axs[0].set_title('Loss')
     axs[0].grid()
-
     axs[1].set_title('Accuracy')
     axs[1].grid()
     return fig
 
 
-if __name__ == '__main__':
+def __load_data_set():
+    global x_train_g, y_train_g, x_test_g, y_test_g
 
-    # load the data set
+    # Load train data
     with np.load('data_train.npz') as data_set:
-        # get the training data
         x_train_g = data_set['x']
         y_train_g = data_set['y']
 
+    # Load test data
     with np.load('data_test.npz') as data_set:
-        # get the test data
         x_test_g = data_set['x']
         y_test_g = data_set['y']
 
     print(f'Training set with {x_train_g.shape[0]} data samples.')
     print(f'Test set with {x_test_g.shape[0]} data samples.')
 
+
+if __name__ == '__main__':
+    __load_data_set()
     tasks = [task1]
 
     pdf = PdfPages('figures.pdf')
@@ -137,4 +171,3 @@ if __name__ == '__main__':
         fig = retval[0] if type(retval) is tuple else retval
         pdf.savefig(fig)
     pdf.close()
-
