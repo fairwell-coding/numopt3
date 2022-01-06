faf541f478910c3cb999958e3110108528dd1bf5
fairwell-coding <coding@temmel.me>
2022-01-06 16:04:19 +0100

    added NAG implementation

--- src/main.py
+++ src/main.py
@@ -14,12 +14,19 @@ y_test_g = np.empty((40,))
 
 
 class NN(object):
-    def __init__(self, num_input: int, num_hidden: int, num_output: int, gradient_method: str, dtype=np.float32):
+    def __init__(self, num_input: int, num_hidden: int, num_output: int, gradient_method: str, momentum=0.0, dtype=np.float32):
         self.num_input = num_input
         self.num_hidden = num_hidden
         self.num_output = num_output
         self.dtype = dtype
         self.gradient_method = gradient_method
+        self.last_gradient_update = 0
+
+        self.momentum = momentum
+        self.last_update_W1 = np.empty((3, 16))
+        self.last_update_b1 = np.empty((3,))
+        self.last_update_W0 = np.empty((16, 4))
+        self.last_update_b0 = np.empty((16,))
 
         self.init_params()
 
@@ -83,12 +90,12 @@ class NN(object):
                 mini_batch_y = self.__encode_output(y_train_g[sample_index])  # converts the target label to the required network output format (i.e. 3 neurons of last layer)
 
                 self.forward(mini_batch_x)
-                self.backward(mini_batch_x, mini_batch_y)
 
-                # optimize network
                 if self.gradient_method == 'GD':
+                    self.backward_GD(mini_batch_x, mini_batch_y)
                     self.steepest_descent(lr)
                 elif self.gradient_method == 'NAG':
+                    self.backward_NAG(mini_batch_x, mini_batch_y)
                     self.nesterov_accelerated_gradient(lr)
 
     def forward(self, x: np.ndarray):
@@ -99,7 +106,7 @@ class NN(object):
 
         return self.layers[1]['output']
 
-    def backward(self, x: np.ndarray, y: np.ndarray):
+    def backward_GD(self, x: np.ndarray, y: np.ndarray):
         """ Perform backwards pass by computing gradients w.r.t. the model parameters (backpropagate errors through the network using partial derivatives via chain-rule)
 
         param x: sample used for current forward pass
@@ -108,12 +115,41 @@ class NN(object):
 
         # Calculate gradients for parameters of output layer
         del_L_a2 = - y / self.layers[1]['output']  # derivative of loss function w.r.t. predicted output (i.e. activation of output layer: a2)
-        delta_1 = self.softmax_derivative(del_L_a2)  # = delta_1, b1 | (del_L / del_a2) * (del_a2 / del_z2): derivative of loss (L) w.r.t. pre-activation of output layer (z2)
+        del_a2_z2 = self.softmax_derivative(self.layers[1]['pre_activation'])  # derivative of activated output of output layer (a2) w.r.t. its weighted input (z2)
+        delta_1 = del_L_a2 * del_a2_z2  # = delta_1, b1 | (del_L / del_a2) * (del_a2 / del_z2): derivative of loss (L) w.r.t. pre-activation of output layer (z2)
         del_L_W1 = np.outer(delta_1, self.layers[0]['output'].T)  # W1 | delta_1 * a1.T: derivative of loss (L) w.r.t. weight matrix of output layer (W1)
 
         # Calculate gradients for parameters of hidden layer
         del_L_a1 = np.matmul(self.layers[1]['W1'].T, delta_1)  # derivative of loss function w.r.t. activated output of hidden layer (i.e. a1)
-        delta_0 = self.softplus_derivative(del_L_a1)  # = delta_2, b0 | (del_L / del_z1): derivative of loss (L) w.r.t. pre-activation of hidden layer (z1)
+        del_a1_z1 = self.softplus_derivative(self.layers[0]['pre_activation'])  # derivative of activated output of hidden layer (a1) w.r.t. its weighted input (z1)
+        delta_0 = del_L_a1 * del_a1_z1  # = delta_2, b0 | (del_L / del_z1): derivative of loss (L) w.r.t. pre-activation of hidden layer (z1)
+        del_L_W0 = np.outer(delta_0, x.T)  # W0 | delta_0 * x.T: derivative of loss (L) w.r.t. weight matrix of hidden layer (W0)
+
+        # Store calculated gradients in their respective network layers
+        self.layers[1]['W1_grad'] = del_L_W1
+        self.layers[1]['b1_grad'] = delta_1
+        self.layers[0]['W0_grad'] = del_L_W0
+        self.layers[0]['b0_grad'] = delta_0
+
+    def backward_NAG(self, x: np.ndarray, y: np.ndarray):
+        """ Perform backwards pass by computing gradients w.r.t. the model parameters (backpropagate errors through the network using partial derivatives via chain-rule). This method calculates the look-ahead
+
+        param x: sample used for current forward pass
+        param y: supervised label
+        """
+
+        # Calculate look-ahead weights
+        W1_look_ahead = self.layers[1]['W1'] - self.momentum * self.last_update_W1
+        b1_look_ahead = self.layers[1]['b1'] - self.momentum * self.last_update_b1
+        W0_look_ahead = self.layers[0]['W0'] - self.momentum * self.last_update_W0
+        b0_look_ahead = self.layers[0]['b0'] - self.momentum * self.last_update_b0
+
+        # Calculate gradients for parameters of output layer
+        delta_1 = b1_look_ahead  # use look-head instead of calculating the current value
+        del_L_W1 = np.outer(delta_1, self.layers[0]['output'].T)  # W1 | delta_1 * a1.T: derivative of loss (L) w.r.t. weight matrix of output layer (W1)
+
+        # Calculate gradients for parameters of hidden layer
+        delta_0 = b0_look_ahead
         del_L_W0 = np.outer(delta_0, x.T)  # W0 | delta_0 * x.T: derivative of loss (L) w.r.t. weight matrix of hidden layer (W0)
 
         # Store calculated gradients in their respective network layers
@@ -133,20 +169,23 @@ class NN(object):
         self.layers[0]['W0'] -= lr * self.layers[0]['W0_grad']
         self.layers[0]['b0'] -= lr * self.layers[0]['b0_grad']
 
-        print('x')  # TODO: Do we need to use normalized gradients for steepest descent?
-
     def nesterov_accelerated_gradient(self, lr):
         """ Optimize network weights based on Nesterov accelerated gradient method to update model weights for current training iteration
 
         param lr: learning rate used for optimization step
         """
 
-        self.layers[1]['W1'] -= lr * self.layers[1]['W1_grad']
-        self.layers[1]['b1'] -= lr * self.layers[1]['b1_grad']
-        self.layers[0]['W0'] -= lr * self.layers[0]['W0_grad']
-        self.layers[0]['b0'] -= lr * self.layers[0]['b0_grad']
+        # update gradients based on look-ahead gradients
+        self.layers[1]['W1'] -= self.momentum * self.last_update_W1 + lr * self.layers[1]['W1_grad']
+        self.layers[1]['b1'] -= self.momentum * self.last_update_b1 + lr * self.layers[1]['b1_grad']
+        self.layers[0]['W0'] -= self.momentum * self.last_update_W0 + lr * self.layers[0]['W0_grad']
+        self.layers[0]['b0'] -= self.momentum * self.last_update_b0 + lr * self.layers[0]['b0_grad']
 
-        print('x')
+        # store last gradient updates needed for next optimization step
+        self.last_update_W1 = self.layers[1]['W1']
+        self.last_update_b1 = self.layers[1]['b1']
+        self.last_update_W0 = self.layers[0]['W0']
+        self.last_update_b0 = self.layers[0]['b0']
 
     def __encode_output(self, y):
         target_label = [0 for i in range(self.num_output)]
@@ -172,16 +211,17 @@ def task1():
     num_hidden = 16
     num_output = 3
 
-    net_GD = NN(num_input, num_hidden, num_output, gradient_method='GD')  # create NN with the steepest gradient descent
     # y_hat = net_GD.forward(x_test_g[0, :])
     # net_GD.backward(y_train_g[0], y_hat)
-    net_GD.train(lr=0.01, epochs=350)  # lr = {0.1, 0.001}
+    # net_GD = NN(num_input, num_hidden, num_output, gradient_method='GD')  # create NN with the steepest gradient descent
+    # net_GD.train(lr=0.01, epochs=350)  # lr = {0.1, 0.001}
 
     # Model using Nesterovs method
-    # net_NAG = NN(num_input, num_hidden, num_output, gradient_method='NAG')
+    net_NAG = NN(num_input, num_hidden, num_output, gradient_method='NAG', momentum=0.9)
+    net_NAG.train(lr=0.01, epochs=350)
 
     # Export models
-    net_GD.export_model()
+    # net_GD.export_model()
     # net_NAG.export_model()
 
     return __create_plots()
